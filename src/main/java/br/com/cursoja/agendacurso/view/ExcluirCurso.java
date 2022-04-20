4a1b30d9b49551fa27e171885da43974ba018b40
Rafael Moura <rafael.g.moura@hotmail.com>
2022-04-20 20:54:00 -0300

    feat: funcao de excluir cursos

---
+++ src/main/java/br/com/cursoja/agendacurso/view/ExcluirCurso.java
@@ -0,0 +1,64 @@
+package br.com.cursoja.agendacurso.view;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import br.com.cursoja.agendacurso.controller.CursoController;
+import br.com.cursoja.agendacurso.entidade.Curso;
+
+/**
+ * Servlet implementation class ExcluirCurso
+ */
+public class ExcluirCurso extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExcluirCurso() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String strId = request.getParameter("id");
+		long id = 0;
+		
+		try {
+			id = Long.parseLong(strId);
+			
+		}catch(Exception e) {
+			System.out.println("Erro na Conversão");
+		}
+		
+		CursoController controller = new CursoController();
+		Curso c = new Curso();
+		c.setId(id);
+		
+		controller.excluir(c);
+				
+				
+		RequestDispatcher rd = request.getRequestDispatcher("listarcursos.jsp");
+		
+		rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
